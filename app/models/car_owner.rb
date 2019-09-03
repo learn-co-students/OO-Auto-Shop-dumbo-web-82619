b0beffc4ea34ddce654e88cc2edb5b746b11ca0d
github username <github email address>
2019-09-03 12:51:10 -0400

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -2,9 +2,32 @@ class CarOwner
 
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def owned_cars
+    Car.all.select do |car|
+      car.owner == self
+    end
+  end
+
+  def mechanics
+    mechanics = self.owned_cars.map do |car|
+      car.mechanic
+    end
+    mechanics.uniq
+  end
+
+  def self.average
+    Car.all.size.to_f / CarOwner.all.size
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,19 @@ def reload
   load 'config/environment.rb'
 end
 
+karina = CarOwner.new("Karina")
+benny = CarOwner.new("Benny")
+
+aibek = Mechanic.new("Aibek", "new")
+theresa = Mechanic.new("Theresa", "foreign")
+liz = Mechanic.new("Liz", "old")
+tom = Mechanic.new("Tom", "foreign")
+
+
+car1 = Car.new("old", karina, aibek)
+car2 = Car.new("new", karina, theresa)
+car3 = Car.new("retro", benny, theresa)
+car4 = Car.new("foreign", karina, theresa)
+car5 = Car.new("new", benny, aibek)
 binding.pry
+0
