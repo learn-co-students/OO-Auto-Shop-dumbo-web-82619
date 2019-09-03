f85064eea812926763533a55c2169eea90f1e052
lsamano <leizl.samano@flatironschool.com>
2019-09-03 13:04:35 -0400

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,38 @@
 class CarOwner
 
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def owned_cars
+    Car.all.select do |car|
+      car.owner == self
+    end
+  end
+
+  def mechanics
+    mechanics_array = self.owned_cars.map do |car|
+      car.mechanic
+    end
+    mechanics_array.uniq
+  end
+
+  def self.average
+    # Car.all.size / CarOwner.all.size
+
+    number_of_owners = CarOwner.all.size
+    array_of_numbers = CarOwner.all.map do |owner|
+      owner.owned_cars.size
+    end
+    array_of_numbers.inject(50, :+) / number_of_owners
+  end
 
 end
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,33 @@
 class Mechanic
 
   attr_reader :name, :specialty
+  @@all = []
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
 
+  def cars
+    Car.all.select do |car|
+      car.mechanic == self
+    end
+  end
+
+  def car_owners
+    cars.map do |car|
+      car.owner
+    end
+  end
+
+  def car_owner_names
+    car_owners.map do |owner|
+      owner.name
+    end
+  end
 end
