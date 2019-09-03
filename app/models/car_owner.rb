30f476117db45ac76ebd22bdadca86e900f05f13
Brett Saxon <brettcsaxon@gmail.com>
2019-09-03 17:51:38 -0400

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -2,9 +2,61 @@ class CarOwner
 
   attr_reader :name
 
+@@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  #class methods will always begin their def with self
+
+  def self.all
+    @@all
+  end
+
+  def cars
+    #self is the oinstance of a carowner who called this function
+    # select is a filter on a condition
+    #[<CarObj>,<CarObj>,<CarObj>]
+    Car.all.select do |car|
+      car.owner == self
+    end
+
+  end
+
+  def mechanics
+    array_of_my_cars = self.cars()
+    array_of_my_cars.map { |car_object| car_object.mechanic }
+  end
+  
+  def mechanic_names
+    self.mechanics().map {|mechanic| mechanic.name}
+  end
+
+
+  def cars_make_and_model
+    self.cars().map do |car|
+      "#{car.make} #{car.model}"
+    end
+
+    
+  end
+
+  def self.average_cars
+    number_of_cars = Car.all.size
+    number_of_owners = CarOwner.all.size
+    number_of_cars.to_f / number_of_owners
+  end
+
+  # self.all
+  # all
+  # @@all
+  # CarOwner.all  all ok to use in class method (above)
+  
+
+
+
 
 
 end
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -2,10 +2,37 @@ class Mechanic
 
   attr_reader :name, :specialty
 
+  @@all = []
+
 def initialize(name, specialty)
   @name = name
   @specialty = specialty
+  @@all << self
 end
+
+def self.all
+  @@all
+end
+
+def cars
+
+  Car.all.select do |car|
+    car.mechanic == self
+
+  end
+end
+
+  def car_owners
+    # array_of_cars = self.cars()
+    self.cars.map do |car|
+      car.owner 
+    end
+  end
+
+  def owner_names
+    self.car_owners.map {|owner| owner.name }
+  end
+
 
 
 end
